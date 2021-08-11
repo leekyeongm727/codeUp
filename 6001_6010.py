11511c392c7c0d69293696283b9f2be2f2af60a3
82109 <dlrudals25@nate.com>
2021-08-12 00:50:28 +0900

    6001~6010

---
+++ 6001_6010.py
@@ -0,0 +1,32 @@
+#6001 -> Hello
+print("Hello")
+
+#6002 -> Hello World
+print("Hello World")
+
+#6003 -> Hello /n World
+print("Hello")
+print("World")
+
+#6004 -> 'Hello'
+print("'Hello'")
+
+#6005 -> "Hello World
+print('"Hello World"')
+
+#6006 -> \"!@#$%^&*()\'
+print("\"!@#$%^&*()\'")
+
+#6007 -> \"C:\\Download\\\'hello\'.py\"
+print("\"C:\\Download\\\'hello\'.py\"")
+
+#6008 -> print(\"Hello\\nWorld\")
+print("print(\"Hello\\nWorld\")")
+
+#6009 -> 문자 1개 입력받아 그대로 출력하기
+x=input()
+print(x)
+
+#6010 -> 정수 1개 입력받아 int로 변환하여 출력하기
+n=int(input())
+print(n)
